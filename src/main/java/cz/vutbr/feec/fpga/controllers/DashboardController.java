b91bc828a18b6d5804aafdfedf6871adc76d53d2
SedaQ <pavelseda@email.cz>
2021-08-18 20:13:26 +0200

    test

--- src/main/java/cz/vutbr/feec/fpga/controllers/DashboardController.java
+++ src/main/java/cz/vutbr/feec/fpga/controllers/DashboardController.java
@@ -4,14 +4,12 @@ import cz.vutbr.feec.fpga.exceptions.ExceptionHandler;
 import de.jensd.fx.glyphs.GlyphsDude;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
-import javafx.scene.input.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-
-import javax.swing.*;
 
 public class DashboardController {
     @FXML
@@ -44,6 +42,13 @@ public class DashboardController {
     public MenuItem exitMenuItem;
     @FXML
     public BorderPane dashboardContent;
+    @FXML
+    public Label headerLogo1;
+    @FXML
+    public Label headerLogo2;
+    @FXML
+    public Label headerLogo3;
+
 
     public DashboardController() {
     }
@@ -51,8 +56,24 @@ public class DashboardController {
     @FXML
     private void initialize() {
         GlyphsDude.setIcon(exitMenuItem, FontAwesomeIcon.CLOSE, "1em");
+        initialSettings.setStyle("-fx-background-color: #FFDC00;");
+        encryptionDecryption.setStyle("-fx-background-color: #AAAAAA;");
+        hashFunction.setStyle("-fx-background-color: #AAAAAA;");
+        signingEdDSA.setStyle("-fx-background-color: #AAAAAA;");
+        signingDilithium.setStyle("-fx-background-color: #AAAAAA;");
+
+        loadIcons();
     }
 
+    private void loadIcons() {
+        Image vutLogoImage = new Image(DashboardController.class.getResourceAsStream("../logos/vut.jpg"));
+        ImageView vutLogo = new ImageView(vutLogoImage);
+        vutLogo.setFitWidth(45);
+        vutLogo.setFitHeight(45);
+        headerLogo1.setGraphic(vutLogo);
+        headerLogo2.setGraphic(vutLogo);
+        headerLogo3.setGraphic(vutLogo);
+    }
 
     public void handleExitMenuItem(ActionEvent event) {
         System.exit(0);
@@ -62,6 +83,11 @@ public class DashboardController {
         try {
             BorderPane root = FXMLLoader.load(getClass().getResource("../fxml/InitialSettings.fxml"));
             dashboardContent.getChildren().setAll(root);
+            initialSettings.setStyle("-fx-background-color: #FFDC00;");
+            encryptionDecryption.setStyle("-fx-background-color: #AAAAAA;");
+            hashFunction.setStyle("-fx-background-color: #AAAAAA;");
+            signingEdDSA.setStyle("-fx-background-color: #AAAAAA;");
+            signingDilithium.setStyle("-fx-background-color: #AAAAAA;");
         } catch (Exception ex) {
             ExceptionHandler.handleException(ex);
         }
@@ -71,6 +97,11 @@ public class DashboardController {
         try {
             BorderPane root = FXMLLoader.load(getClass().getResource("../fxml/EncryptionDecryptionContent.fxml"));
             dashboardContent.getChildren().setAll(root);
+            initialSettings.setStyle("-fx-background-color: #AAAAAA;");
+            encryptionDecryption.setStyle("-fx-background-color: #FFDC00;");
+            hashFunction.setStyle("-fx-background-color: #AAAAAA;");
+            signingEdDSA.setStyle("-fx-background-color: #AAAAAA;");
+            signingDilithium.setStyle("-fx-background-color: #AAAAAA;");
         } catch (Exception ex) {
             ExceptionHandler.handleException(ex);
         }
